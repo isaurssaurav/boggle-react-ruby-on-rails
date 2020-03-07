d3d745101db82f18e90a3073a660f68040e88860
saurssauravjs <saurav.maharjan@fusemachines.com>
2020-03-07 21:48:52 +0545

    :white_check_mark: game_helper's method test

--- test/controllers/api/v1/game_controller_test.rb
+++ test/controllers/api/v1/game_controller_test.rb
@@ -3,10 +3,14 @@ require 'test_helper'
 class Api::V1::GameControllerTest < ActionDispatch::IntegrationTest
   include Api::V1::GameHelper
 
-  test "should get index" do
-    html = initialize_game()
-    p html
-    assert true
+  test 'get : /initialize_game' do
+    get "/api/v1/initialize_game"
+    assert_response :success
   end
 
+
+  test 'get : /initialize_board' do
+    get "/api/v1/initialize_board"
+    assert_response :success
+  end
 end
